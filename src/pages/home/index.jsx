e851e1df2edb384e4b60459c8ed123f6cf68046c
zhoushuquan <zhousq@fosun.com>
2021-12-02 17:45:06 +0800

    第一次提交

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const Home = ()=>{
+  return (
+    <div>123569</div>
+  )
+}
+
+export default Home;
---
+++ src/router/index.js
@@ -0,0 +1,8 @@
+import Home from '../pages/home/index.jsx';
+import List from '../pages/list'
+const router = [
+  {path:'/home',name:'home',element:Home},
+  {path:'/list',name:'list',element:List}
+]
+
+export default router;
